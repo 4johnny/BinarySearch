6fbe605448ccbaf0cc091f811d9984b003f589b7
4johnny <4johnny@users.noreply.github.com>
2015-01-13 16:37:54 -0800

    Initial Commit

---
+++ BinarySearch/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BinarySearch
+//
+//  Created by Johnny on 2015-01-13.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
